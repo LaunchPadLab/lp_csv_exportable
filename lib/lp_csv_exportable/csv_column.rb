586e0ae2361dbc5e389f42169837572604d83a6e
Rachel Killackey <rachel@launchpadlab.com>
2017-08-07 17:01:42 -0500

    add attribute, return if present

--- lib/lp_csv_exportable/csv_column.rb
+++ lib/lp_csv_exportable/csv_column.rb
@@ -1,17 +1,19 @@
 module LpCSVExportable
   class CSVColumn
-    attr_reader :header, :model_method, :model_methods, :type
+    attr_reader :header, :default_value, :model_method, :model_methods, :type
 
     def initialize(args = {})
       @header = args[:header]
       @model_method = args[:model_method]
       @model_methods = args[:model_methods]
       @type = args.fetch(:type, :string)
+      @default_value = args.fetch(:default_value, '')
       after_init(args)
     end
 
     def format(result)
       return send(type, result) if respond_to?(type)
+      return default_value if result.nil? && default_value.present?
       result
     end
 
